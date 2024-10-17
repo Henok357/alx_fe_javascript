2343a3dadbaf45d61e024b4f853d5f677a94a8d2
Henok <henokbelete357@gmail.com>
2024-10-16 22:02:20 -0700

    modified dynamic quote generator functionality

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -1,24 +1,35 @@
+
 let quotes = [
     { text: "Life is what happens when you're busy making other plans.", category: "Life" },
     { text: "Get busy living or get busy dying.", category: "Motivation" },
-   
+    { text: "The purpose of our lives is to be happy.", category: "Happiness" },
 ];
 
-function showRandomQuote() {
+
+function displayRandomQuote() {
     const randomIndex = Math.floor(Math.random() * quotes.length);
     const quoteDisplay = document.getElementById('quoteDisplay');
     quoteDisplay.innerText = quotes[randomIndex].text;
 }
 
+
 function addQuote() {
-    const quoteText = document.getElementById('newQuoteText').value;
-    const quoteCategory = document.getElementById('newQuoteCategory').value;
+    const quoteText = document.getElementById('newQuoteText').value.trim();
+    const quoteCategory = document.getElementById('newQuoteCategory').value.trim();
+
+   
     if (quoteText && quoteCategory) {
+       
         quotes.push({ text: quoteText, category: quoteCategory });
+
+      
         document.getElementById('newQuoteText').value = '';
         document.getElementById('newQuoteCategory').value = '';
+
+        
+        displayRandomQuote();
     }
 }
 
 
-document.getElementById('newQuote').addEventListener('click', showRandomQuote);
+document.getElementById('newQuote').addEventListener('click', displayRandomQuote);
