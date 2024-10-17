812f792316325dca5006d980fe7da53280d4579a
Henok <henokbelete357@gmail.com>
2024-10-16 21:57:59 -0700

    Add dynamic quote generator functionality

---
+++ dom-manipulation/script.js
@@ -0,0 +1,24 @@
+let quotes = [
+    { text: "Life is what happens when you're busy making other plans.", category: "Life" },
+    { text: "Get busy living or get busy dying.", category: "Motivation" },
+   
+];
+
+function showRandomQuote() {
+    const randomIndex = Math.floor(Math.random() * quotes.length);
+    const quoteDisplay = document.getElementById('quoteDisplay');
+    quoteDisplay.innerText = quotes[randomIndex].text;
+}
+
+function addQuote() {
+    const quoteText = document.getElementById('newQuoteText').value;
+    const quoteCategory = document.getElementById('newQuoteCategory').value;
+    if (quoteText && quoteCategory) {
+        quotes.push({ text: quoteText, category: quoteCategory });
+        document.getElementById('newQuoteText').value = '';
+        document.getElementById('newQuoteCategory').value = '';
+    }
+}
+
+
+document.getElementById('newQuote').addEventListener('click', showRandomQuote);
